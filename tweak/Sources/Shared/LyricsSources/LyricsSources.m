6010e2ec0efb5e5f2e783edc0e3b27f0fdfde70c
Charles L <Charles_L@outlook.jp>
2026-09-26 00:41:02 +0800

    更新 LyricsSources.m

--- tweak/Sources/Shared/LyricsSources/LyricsSources.m
+++ tweak/Sources/Shared/LyricsSources/LyricsSources.m
@@ -156,8 +156,8 @@ NSArray<SGLyricsProvider *> *SGLyricsAllProviders(void) {
             make(@"binilyrics", @"BiniLyrics", @"Apple Music word timing", SGBiniLyricsAsk),
             make(@"musixmatch", @"Musixmatch", @"Spotify's licensed catalogue", SGMusixmatchAsk),
             make(@"unison", @"Unison", @"Hand-timed, few tracks", SGUnisonAsk),
-            make(@"qqmusic", @"QQ Music", @"Line and word timing", SGQQMusicAsk),
             make(@"netease", @"NetEase", @"Word timing, censored", SGNetEaseAsk),
+            make(@"qqmusic", @"QQ Music", @"Word timing, Chinese translation", SGQQMusicAsk),
             make(@"lrclib", @"LRCLIB", @"Line timing, open fallback", SGLrcLibAsk),
         ];
     });
